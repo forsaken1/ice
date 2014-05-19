bb11c2fad123cae63651ba834d9bd6c97ca934e8
Forsaken <alexey2142@mail.ru>
2014-05-20 08:24:33 +1100

    вынес

--- data_protection/lib/lib.rb
+++ data_protection/lib/lib.rb
@@ -1,5 +1,44 @@
 require 'gtk2'
 
+def parse_str(str, ret = false)
+  dec = ['+', '-', '*', '/', '^', ')']
+  level = 0
+  i = 0
+  s = str.split('')
+  lvls = []
+  ts = []
+
+  while i < s.length
+    if dec.include?(s[i])
+      ts << s[i]
+      level -= 1
+    elsif s[i] == '('
+      ts << s[i]
+      level += 1
+    elsif '0' <= s[i] && s[i] <= '9'
+      level += 1
+      j = i
+      ns = ''
+      while '0' <= s[j] && s[j] <= '9' && j < s.length
+        ns << s[j]
+        j += 1
+      end
+      i = j - 1
+      ts << ns.to_i
+    else
+      return 'unexpected sym'
+    end
+    lvls << level
+    i += 1
+  end
+
+  if ret
+    return [ts, lvls]
+  else
+    return ts
+  end
+end
+
 def get_text(tw)
   start_iter, end_iter, selected  = tw.buffer.selection_bounds
 
