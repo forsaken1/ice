ef526bf3ba00bb25d1c772727811e8ed206ff02c
Forsaken <alexey2142@mail.ru>
2014-06-03 02:10:58 +1100

    Доделал 1.2

--- data_protection/hw1/two.rb
+++ data_protection/hw1/two.rb
@@ -3,14 +3,26 @@ require '../lib/gtk.rb'
 
 ROUND = 5
  
-def encode(data)
-  seq = {}
-  lit = {}
+def encode(data, pattern = data)
+  seq, lit = {}, {}
+  left, right = 0, 1
+
   data.each_char { |e| seq[e] = Float(data.count(e)).round(ROUND) / data.length }
-  seq.sort { |a, b| b[1] <=> a[1] }.each { |e| lit[e[0]] = e[1] }
-  lit
+  seq.sort { |a, b| b[1] <=> a[1] }
+  	.each { |e| lit[e[0]] = [left.round(ROUND), Float(e[1] + left).round(ROUND)]; left += e[1] }
+  
+  new_left, new_right = lit[pattern[0]][0], lit[pattern[0]][1]
+  result = []
+  
+  pattern.each_char do |e|
+  	new_rl = [new_left + (new_right - new_left) * Float(lit[e][0]), 
+  			  		new_left + (new_right - new_left) * Float(lit[e][1])]
+  	new_left, new_right = new_rl[0], new_rl[1]
+  	result << new_rl
+  end
+  result.last.last.to_s
 end
 
-print encode("ABCDB"), "\n"
+print encode("ABCDB", "ABCDB"), "\n"
 
-#init("Arithm. encoding") { |text| encode(text) }
+init("Arithm. encoding") { |text| encode(text) }
