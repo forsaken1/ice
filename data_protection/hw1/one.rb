0a7c2e04f93b14921853c21f8787291cb0963935
Forsaken <a.krylov@espley.ru>
2014-06-02 15:44:59 +1100

    Тесткейс

--- data_protection/hw1/one.rb
+++ data_protection/hw1/one.rb
@@ -1,4 +1,5 @@
 require '../lib/lib.rb'
+#require '../lib/gtk.rb'
 
 def encode(data)
   encode_str = ''
@@ -11,18 +12,21 @@ def encode(data)
   while i < data.length
     current_char = data[i]
     next_char = data[i + 1]
+    cnt += 1
 
-    if current_char == next_char
+    if next_char == nil
+      encode_str += "#{cnt + 1}#{pattern}" if sequence
+      encode_str += "-#{cnt}#{pattern+current_char}" unless sequence
+    elsif current_char == next_char
+      sequence = true if (data[i - 1] == data[i - 2]) && i > 1
       if sequence
         pattern = current_char
-        #encode_str += "#{cnt+1}#{pattern}" if i + 1 == data.length
       else
         sequence = true
         unless start
-          encode_str += "-#{cnt}#{pattern}"
+          encode_str += "-#{cnt - 1}#{pattern}"
           cnt = 0
           pattern = ''
-          next
         end
       end
     else
@@ -31,28 +35,26 @@ def encode(data)
         cnt = 0
         pattern = ''
         sequence = false
-        next
       else
         pattern += current_char
-        #encode_str += "-#{cnt+1}#{pattern}" if i + 1 == data.length
       end
     end
-
-    cnt += 1
     i += 1
     start = false
   end
   encode_str
 end
 
-print 'tests: '
-print encode('') == '' ? '+' : '-'
-print encode('wwwwwwrrrrrrrrgggggggggg') == '6w8r10g' ? '+' : '-'
-print encode('ertertertffff') == '-9ertertert4f' ? '+' : '-'
-print encode('tffff') == '-1t4f' ? '+' : '-'
-print encode('tfffft') == '-1t4f-t' ? '+' : '-'
-print encode('eeeertttt') == '4e1r4t' ? '+' : '-'
-print encode('eeeerthgjf') == '4e-6rthgjf' ? '+' : '-'
-print "\n"
+test_case do
+  errors = ''
+  errors += test('wwwwwwrrrrrrrrgggggggggg', '6w8r10g') { |str| encode(str) }
+  errors += test('ertertertffff', '-9ertertert4f') { |str| encode(str) }
+  errors += test('tffff', '-1t4f') { |str| encode(str) }
+  errors += test('tfffft', '-1t4f-1t') { |str| encode(str) }
+  errors += test('eeeertttt', '4e-1r4t') { |str| encode(str) }
+  errors += test('eeeerthgjf', '4e-6rthgjf') { |str| encode(str) }
+  errors += test('wwwtttr', '3w3w-1r') { |str| encode(str) }
+  errors += test('ntwww', '-2nt3w') { |str| encode(str) }
+end
 
-init("Run-length encoding") { |text| encode(text) }
+#init("Run-length encoding") { |text| encode(text) }
--- data_protection/lib/lib.rb
+++ data_protection/lib/lib.rb
@@ -1,4 +1,19 @@
-require 'gtk2'
+def test_case
+  print 'tests: '
+  errors = yield
+  print "\n"
+  print errors
+end
+
+def test(question, answer)
+  if (res = yield(question)) == answer
+    print '+'
+    ''
+  else
+    print '-'
+    "Error: #{question} != #{answer}, but: #{res}\n"
+  end
+end
 
 def execute(a, sign, b)
   if sign == '+'
@@ -51,54 +66,4 @@ def parse_str(str, ret = false)
   else
     return ts
   end
-end
-
-def get_text(tw)
-  start_iter, end_iter, selected  = tw.buffer.selection_bounds
-
-  if !selected
-    start_iter, end_iter = tw.buffer.bounds
-  end
-  tw.buffer.get_text(start_iter, end_iter)
-end
-
-def insert_text(text_out, text)
-  text_out.buffer.text = (text)
-end
-
-def init(title)
-  window = Gtk::Window.new
-  window.set_title(title)
-  window.border_width = 10
-  window.resizable = true
-  window.set_size_request(600, 300)
-
-  text_in = Gtk::TextView.new
-  text_out = Gtk::TextView.new
-
-  button = Gtk::Button.new("Encode")
-  button.signal_connect("clicked") do
-    text = get_text(text_in)
-    insert_text(text_out, yield(text))
-  end
-
-  hbox = Gtk::HBox.new(false, 5)
-  hbox.pack_start_defaults(button)
-  vbox = Gtk::VBox.new(false, 5)
-  vbox.pack_start(text_in,  true,  true, 0)
-  vbox.pack_start(text_out, true,  true, 0)
-  vbox.pack_start(hbox,     false, true, 0)
-
-  window.signal_connect("delete_event") do
-    false
-  end
-
-  window.signal_connect("destroy") do
-    Gtk.main_quit
-  end
-
-  window.add(vbox)
-  window.show_all
-
-  Gtk.main
 end
