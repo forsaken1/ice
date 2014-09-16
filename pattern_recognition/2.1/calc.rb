4ed249d567291db52cb3a3a4be07b45a27e09d5b
Forsaken <alexey2142@mail.ru>
2014-09-16 22:25:47 +1100

    Подкорректировал вычисление ряда распределения

--- pattern_recognition/2.1/calc.rb
+++ pattern_recognition/2.1/calc.rb
@@ -1,14 +1,23 @@
-def mult_by_range(m, n)
+def fact(n)
   result = Float(1)
-  (m..n).each { |i| result *= i }
+  (1..n).each { |i| result *= i }
   result
+end
+
+def combination(m, n) # сочетания из n по m
+  fact(n) / ( fact(m) * fact(n - m) )
+end
+
+def calc(_m, _n, m, n)
+  ( combination(m, _m) * combination(n - m, _n - _m) ) / combination(n, _n)
 end
 
 arr = []
 i = 0
 while i <= 20
-  arr << ( mult_by_range(61 + i, 80) * mult_by_range(21 - i, 20) ) / mult_by_range(81, 100)
+  arr << calc(20, 100, i, 20)
+  p "p(#{i}) = " + arr.last.to_s
   i += 1
 end
 
-p arr
+p "sum p_i = " + arr.inject(0) { |sum, x| sum + x }.to_s
