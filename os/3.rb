8241017ca939f84e81d090cb650d22e79dc3d9fe
Forsaken <alexey2142@mail.ru>
2014-10-28 08:26:48 +1100

    Поправил баг с cd ..

--- os/3.rb
+++ os/3.rb
@@ -1,4 +1,5 @@
 require '../data_protection/lib/gtk.rb'
+require '../data_protection/lib/lib.rb'
 
 $sym_table = {}
 
@@ -12,7 +13,7 @@ def execute text
       Dir.chdir
       "Directory changed: #{Dir.pwd}"
     when /^ *cd *[\w.]+ *$/
-      text.gsub(/ *cd *([\w]*) */) { Dir.chdir $1 }
+      text.gsub(/ *cd *([\w.]*) */) { Dir.chdir $1 }
       "Directory changed: #{Dir.pwd}"
     when /^ *mkdir *[\w]+ *$/
       text.gsub(/ *mkdir *([\w]+) */) { Dir.mkdir $1 }
@@ -22,8 +23,11 @@ def execute text
       "Directory removed: #{$1}"
     when /^ *help *$/
       "Available commands: pwd, ls, cd <name dir>, mkdir <name dir>, rmdir <name dir>,\n echo <var>, set <var> = <value>"
-    when /^ *set *[\w_]+ *= *["\w_]+ *$/
-      text.gsub(/^ *set *([\w_])+ *= *(["\w_])+ *$/) { $sym_table[$1] = $2 }
+    when /^ *set *[\w_]+ *= *[\d]+ *$/
+      text.gsub(/^ *set *([\w_])+ *= *([\d])+ *$/) { $sym_table[$1] = $2 }
+      "#{$1} = #{$2}"
+    when /^ *set *[\w_]+ *= *["']{1}([\w_]+)['"]{1} *$/
+      text.gsub(/^ *set *([\w_]+) *= *["']{1}([\w_]+)['"]{1} *$/) { $sym_table[$1] = $2 }
       "#{$1} = #{$2}"
     when /^ *echo *[\w_]+ *$/
       text.gsub(/^ *echo *([\w_])+ *$/) { $sym_table[$1] }
@@ -37,6 +41,11 @@ def shell text
   operations = text.split '|'
   operations.each { |operation| output += execute(operation) + "\n" }
   output
+end
+
+test_case do
+  errors = ''
+  errors += test('cd', 'Directory changed: /home/forsaken') { |text| shell text }
 end
 
 init('EmulateShell') { |text| shell text }
