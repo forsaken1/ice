3c2417ac138cf05987c8be344f75ee0ab67fc720
Forsaken <alexey2142@mail.ru>
2014-11-06 03:36:33 +1000

    Тесты

--- os/3.rb
+++ os/3.rb
@@ -3,6 +3,20 @@ require '../data_protection/lib/lib.rb'
 
 $sym_table = {}
 
+class Dir
+  def self.rmdir_rec path
+    Dir.chdir path
+    Dir.entries(Dir.pwd).each do |item|
+      if Dir.exists?(item)
+        Dir.rmdir_rec item
+        Dir.rmdir item
+      else
+      end
+    end
+    Dir.chdir '..'
+  end
+end
+
 def execute text
   case text
     when /^ *pwd *$/
@@ -13,24 +27,27 @@ def execute text
       Dir.chdir
       "Directory changed: #{Dir.pwd}"
     when /^ *cd *[\w.]+ *$/
-      text.gsub(/ *cd *([\w.]*) */) { Dir.chdir $1 }
+      text.gsub(/ *cd *([\w.]+) */) { Dir.chdir $1 }
       "Directory changed: #{Dir.pwd}"
     when /^ *mkdir *[\w]+ *$/
-      text.gsub(/ *mkdir *([\w]+) */) { Dir.mkdir $1 }
+      text.gsub(/ *mkdir *([\w]+) */) { Dir.mkdir $1 unless Dir.exists? $1 }
       "Directory created: #{$1}"
     when /^ *rmdir *[\w]+ *$/
-      text.gsub(/ *rmdir *([\w]+) */) { Dir.rmdir $1 }
+      text.gsub(/ *rmdir *([\w]+) */) { Dir.rmdir_rec $1 if Dir.exists? $1 }
       "Directory removed: #{$1}"
-    when /^ *help *$/
-      "Available commands: pwd, ls, cd <name dir>, mkdir <name dir>, rmdir <name dir>,\n echo <var>, set <var> = <value>"
     when /^ *set *[\w_]+ *= *[\d]+ *$/
-      text.gsub(/^ *set *([\w_])+ *= *([\d])+ *$/) { $sym_table[$1] = $2 }
+      text.gsub(/^ *set *([\w_]+) *= *([\d]+) *$/) { $sym_table[$1] = $2 }
       "#{$1} = #{$2}"
     when /^ *set *[\w_]+ *= *["']{1}([\w_]+)['"]{1} *$/
       text.gsub(/^ *set *([\w_]+) *= *["']{1}([\w_]+)['"]{1} *$/) { $sym_table[$1] = $2 }
       "#{$1} = #{$2}"
     when /^ *echo *[\w_]+ *$/
       text.gsub(/^ *echo *([\w_])+ *$/) { $sym_table[$1] }
+    when /^ *help *$/
+      <<-HELP
+      Available commands: pwd, ls, cd <name dir>, mkdir <name dir>, rmdir <name dir>,
+      echo <var>, set <var> = <integer>, set <var> = "<string>"
+      HELP
     else
       "Parse error"
   end
@@ -39,13 +56,24 @@ end
 def shell text
   output = ''
   operations = text.split '|'
-  operations.each { |operation| output += execute(operation) + "\n" }
+  operations.each { |operation| output += "#{execute(operation)}\n" }
   output
 end
 
-test_case do
-  errors = ''
-  errors += test('cd', 'Directory changed: /home/forsaken') { |text| shell text }
+if ARGV.first == '-t'
+  test_case do
+    errors = ''
+    errors += test('cd', "Directory changed: /home/forsaken\n") { |text| shell text }
+    errors += test('pwd', "Current directory: /home/forsaken\n") { |text| shell text }
+    errors += test('mkdir test_dir', "Directory created: test_dir\n") { |text| shell text }
+    errors += test('cd test_dir', "Directory changed: /home/forsaken/test_dir\n") { |text| shell text }
+    errors += test('mkdir test', "Directory created: test\n") { |text| shell text }
+    errors += test('ls', "..   .   test\n") { |text| shell text }
+    errors += test('cd', "Directory changed: /home/forsaken\n") { |text| shell text }
+    errors += test('rmdir test_dir', "Directory removed: test_dir\n") { |text| shell text }
+    errors += test('set a = 42', "a = 42\n") { |text| shell text }
+    errors += test('echo a', "42\n") { |text| shell text }
+  end
+else
+  init('EmulateShell') { |text| shell text }
 end
-
-init('EmulateShell') { |text| shell text }
