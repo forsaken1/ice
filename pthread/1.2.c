137aca612753a1581da66f88558643e569aed379
Forsaken <alexey2142@mail.ru>
2014-11-13 03:32:19 +1000

    Начал 1.2

--- pthread/1.2.c
+++ pthread/1.2.c
@@ -1,41 +1,39 @@
 #include <stdio.h>
 #include <pthread.h>
 
-static void *third_thread_func(void *i)
+pthread_mutex_t mutex_param;
+
+static void *third_thread_func()
 {
 	printf("Thread three: %d\n", (int)i);
 	pthread_exit(NULL);
 }
 
-static void *second_thread_func(void *i)
+static void *second_thread_func()
 {
 	pthread_t thread;
 
 	if((int)i % 2 == 0)
 	{
-		if(!pthread_create(&thread, NULL, third_thread_func, i))
+		if(!pthread_create(&thread, NULL, third_thread_func, NULL))
 		{
 			printf("Error: thread not created\n");
 		}
 	}
 	else
 	{
-		printf("Thread two: %d\n", (int)i);
+		printf("Thread two: %d\n", mutex_param);
 	}
 	pthread_exit(NULL);
 }
 
 static void *first_thread_func()
 {
-	int i;
 	pthread_t thread;
 
-	for(i = 1; i <= 100; ++i)
+	if(!pthread_create(&thread, NULL, second_thread_func, NULL))
 	{
-		if(!pthread_create(&thread, NULL, second_thread_func, (void *)i))
-		{
-			printf("Error: thread not created\n");
-		}
+		printf("Error: thread not created\n");
 	}
 	pthread_exit(NULL);
 }
@@ -44,9 +42,12 @@ int main()
 {
 	pthread_t thread;
 
+	pthread_mutex_init(&mutex_param, 0);
+
 	if(!pthread_create(&thread, NULL, first_thread_func, NULL))
 	{
 		printf("Error: thread not created\n");
 	}
+	pthread_mutex_destroy(&mutex_param);
 	pthread_exit(NULL);
 }
